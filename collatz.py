550d802b0765d9467ca550e8b845c4a08fb6d423
DiceTJ <61960729+DiceTJ@users.noreply.github.com>
2022-02-21 14:13:24 -0700

    Updated Main Code to include comments
    Improved Readability

--- collatz.py
+++ collatz.py
@@ -1,18 +1,18 @@
-startNum=int(input('Enter a number to be Collatz\'d: '))
-jumps=int(0)
-currentNum=int(startNum)
-while currentNum!=1:
+startNum=int(input('Enter a number to be Collatz\'d: ')) #Ask the User for a number
+jumps=int(0) #Start the counter for jumps at zero
+currentNum=int(startNum) #Assign the starting number to the current number
+while currentNum!=1: #Start the primary loop
 
-  if currentNum % 2 == 0:
-    print('Even')
-    currentNum=currentNum/2
-    print(currentNum)
+  if currentNum % 2 == 0: #Assess if the number is Even
+    print('Even') #Show the current step
+    currentNum=currentNum/2 #Do math
+    print(currentNum) #Show the new number
   else:
-    print('Odd')
-    currentNum=(currentNum*3)+1
-    print(currentNum)
-  jumps+=1
+    print('Odd') #Print that the number is Odd
+    currentNum=(currentNum*3)+1 #Do the math
+    print(currentNum) #Show the new Number
+  jumps+=1 #Add one to the jumps counter
 
-print('Starting Number: ',int(startNum))
-print('Ending Number: ',int(currentNum))
-print('Number of Jumps: ',jumps)
+print('Starting Number: ',int(startNum)) #Print total jumps at the end
+print('Ending Number: ',int(currentNum)) #Print ending number at the end (Should be 1)
+print('Number of Jumps: ',jumps) #Print number of jumps
